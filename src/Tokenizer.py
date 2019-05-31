baad72c308ca98f808b0d27b7003bf0a1532f1de
Omar Valenzuela <ovalenzu@uci.edu>
2019-05-30 21:57:07 -0700

    reverted to backup

--- src/Tokenizer.py
+++ src/Tokenizer.py
@@ -8,38 +8,39 @@ from bs4 import BeautifulSoup
 import nltk
 from nltk.tokenize import RegexpTokenizer
 from _collections import defaultdict
+import os
 from _contextvars import Token
 from lxml.html.diff import token
 
 class Tokenizer:
-
+    
     def generateFilePath(self, folderNum, fileNum):
         return f"../WEBPAGES_RAW/{folderNum}/{fileNum}"
-
-
-
+    
+        
+    
     def readFile(self, fileName):
         try:
             oFile = open(fileName, 'r')
             text = oFile.read()
             return text.encode('utf-8')
-
+        
         finally:
             oFile.close()
-
-
+            
+            
     def tokenizeFile(self, rawFile):
         #return a list of tokens
         soup = BeautifulSoup(rawFile, 'lxml')
         tokens = nltk.word_tokenize(soup.get_text().lower())
         return [w.lower() for w in tokens if w.isalnum() and not any(char.isdigit() for char in w)]
-
-
+    
+    
     def populateTokenDictFrequencies(self, tokens):
         tokenDict = defaultdict(int)
         for token in tokens:
             tokenDict[token] += 1
-
+            
         return tokenDict
 
     def generateTokenDict(self, folderNum, fileNum):
@@ -49,8 +50,11 @@ class Tokenizer:
             tokens = self.tokenizeFile(rawFile)
             tokenFreq = self.populateTokenDictFrequencies(tokens)
             return tokenFreq
-
-
-
+        
+        
+        
 # t = Tokenizer()
 # print(t.generateTokenDict(0, 4))
+    
+    
+        
