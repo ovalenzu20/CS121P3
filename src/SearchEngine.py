ae2e6127d33aae2d5d6093199a623ee2f5f4da2b
Omar Valenzuela <ovalenzu@uci.edu>
2019-05-31 01:30:59 -0700

    final changes

--- src/SearchEngine.py
+++ src/SearchEngine.py
@@ -8,30 +8,34 @@ from Tokenizer import Tokenizer
 from InvertedIndex import InvertedIndex
 from _collections import defaultdict
 
+
 class SearchEngine:
     
     def __init__(self):
         self.query = None
-
-    def run(self):
-        II = InvertedIndex()
-        T = Tokenizer()
-        invertedIndex = II.loadPickle(II.pickleName)
-        urls = self.getJSON("../WEBPAGES_RAW/bookkeeping.json")
+        self.urls = self.getJSON("../WEBPAGES_RAW/bookkeeping.json")
+        self.II = InvertedIndex()
+        self.T = Tokenizer()
         
+    def run(self):
+        invertedIndex = InvertedIndex.loadPickle(InvertedIndex.pickleName)
         userInput = input("Please enter a query: ")
         self.query = userInput
         print("To end the Search Engine, please enter the letter 'q'. \n")
         
         while userInput != "q":
+            self.search(self.query, invertedIndex)
             
-            tokens = T.tokenizeText(userInput)
-            documentScores = self.getRelevantDocScores(tokens, invertedIndex)
-            self.displayTopKSearchResults(documentScores, urls, 20)
-            print("To end the Search Engine, please enter the letter 'q'. \n")
-
             userInput = input("Please enter a query: ")
             self.query = userInput
+            print("To end the Search Engine, please enter the letter 'q'. \n")
+
+            
+            
+    def search(self, query, invertedIndex):
+        tokens = self.T.tokenizeText(query)
+        documentScores = self.getRelevantDocScores(tokens, invertedIndex)
+        self.displayTopKSearchResults(documentScores, self.urls, 20)
             
     
     def getRelevantDocScores(self, tokens, invertedIndex):
@@ -46,13 +50,13 @@ class SearchEngine:
     def displayTopKSearchResults(self, documentScores, urls, K):
         decreasingOrder = sorted(documentScores, key = lambda val: documentScores[val], reverse = True)[:K]
         ranking = 1
+        print(f"Total results found: {len(documentScores)}")
         print(f'Displaying top {len(decreasingOrder)} results for the query "{self.query}":')
         for filePath in decreasingOrder:
             refinedPath = self.refineFilePath(filePath)
             print(f"\t{ranking}. {urls[refinedPath]}")
             ranking += 1
         
-            
             
     def getJSON(self, path):
         try:
@@ -68,7 +72,35 @@ class SearchEngine:
         return splitPath[2] + '/' + splitPath[3]
  
  
+    def outputStats(self):
+        invertedIndex = self.II.loadPickle(self.II.pickleName)
+        queries = ["Informatics", "Mondego", "Irvine", "artificial intelligence", "computer science"]
+        milestone2 = open("Milestone2.txt", 'w+')
+
+        for query in queries:
+            self.query = query
+            tokens = self.T.tokenizeText(query)
+            documentScores = self.getRelevantDocScores(tokens, invertedIndex)
+            self.writeResults(milestone2, documentScores, self.urls, 20)
+            
+        milestone2.close()
+    
+    def writeResults(self, fileToWrite, documentScores, urls, K):
+        decreasingOrder = sorted(documentScores, key = lambda val: documentScores[val], reverse = True)[:K]
+        ranking = 1
+        
+        fileToWrite.write(f"Total results found: {len(documentScores)} \n")
+        fileToWrite.write(f'Displaying top {len(decreasingOrder)} results for the query "{self.query}":\n')
+        for filePath in decreasingOrder:
+            refinedPath = self.refineFilePath(filePath)
+            fileToWrite.write(f"\t{ranking}. {urls[refinedPath]}\n")
+            ranking += 1
+            
+        fileToWrite.write("\n")
+            
+ 
 if __name__ == "__main__":
     s = SearchEngine()
-    s.run()
+#     s.run()
+    s.outputStats()
     
